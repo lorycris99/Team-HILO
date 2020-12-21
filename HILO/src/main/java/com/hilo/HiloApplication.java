61869323bd9ede268d58f632aed49fe7d017ec76
lost99 <ivancadamo@gmail.com>
2020-12-21 12:23:10 +0100

    Spring initzializer

---
+++ HILO/src/main/java/com/hilo/HiloApplication.java
@@ -0,0 +1,13 @@
+package com.hilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiloApplication.class, args);
+	}
+
+}

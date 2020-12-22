78471458c6015a195ad97154219c439209914d44
h3rm4nn-99 <73171414+h3rm4nn-99@users.noreply.github.com>
2020-12-22 13:02:57 +0100

    Update Links.java

--- HILO/src/main/java/com/hilo/model/statisticsmanagement/Links.java
+++ HILO/src/main/java/com/hilo/model/statisticsmanagement/Links.java
@@ -1,8 +1,5 @@
 package com.hilo.model.statisticsmanagement;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Links {
   public static final String GET_STATISTICS = "/home";
 }

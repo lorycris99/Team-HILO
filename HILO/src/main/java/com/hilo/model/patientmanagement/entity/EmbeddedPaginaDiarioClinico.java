406a1425a7d8f0f0adf4fab97fb1972f0dc4ecc0
xXViRtUaLcObRaXx <65489866+xXViRtUaLcObRaXx@users.noreply.github.com>
2020-12-22 11:26:56 +0100

    TEST

--- HILO/src/main/java/com/hilo/model/patientmanagement/entity/EmbeddedPaginaDiarioClinico.java
+++ HILO/src/main/java/com/hilo/model/patientmanagement/entity/EmbeddedPaginaDiarioClinico.java
@@ -1,68 +1,78 @@
 package com.hilo.model.patientmanagement.entity;
 
 import java.io.Serializable;
-
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class EmbeddedPaginaDiarioClinico implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    
-    public EmbeddedPaginaDiarioClinico() {}
+  private static final long serialVersionUID = 1L;
 
-    private Integer numero;
-    private String cfPaziente;
+  public EmbeddedPaginaDiarioClinico() {}
 
-    public Integer getNumero() {
-        return numero;
+  private Integer numero;
+  private String cfPaziente;
+
+  public Integer getNumero() {
+
+    return numero;
+  }
+
+  public void setNumero(Integer numero) {
+
+    this.numero = numero;
+  }
+
+  public String getCfPaziente() {
+
+    return cfPaziente;
+  }
+
+  public void setCfPaziente(String cfPaziente) {
+
+    this.cfPaziente = cfPaziente;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((cfPaziente == null) ? 0 : cfPaziente.hashCode());
+    result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
     }
-
-    public void setNumero(Integer numero) {
-        this.numero = numero;
+    if (obj == null) {
+      return false;
     }
-
-    public String getCfPaziente() {
-        return cfPaziente;
+    if (getClass() != obj.getClass()) {
+      return false;
     }
-
-    public void setCfPaziente(String cfPaziente) {
-        this.cfPaziente = cfPaziente;
+    EmbeddedPaginaDiarioClinico other = (EmbeddedPaginaDiarioClinico) obj;
+    if (cfPaziente == null) {
+      if (other.cfPaziente != null) {
+        return false;
+      }
+    } else if (!cfPaziente.equals(other.cfPaziente)) {
+      return false;
     }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((cfPaziente == null) ? 0 : cfPaziente.hashCode());
-        result = prime * result + ((numero == null) ? 0 : numero.hashCode());
-        return result;
+    if (numero == null) {
+      if (other.numero != null) {
+        return false;
+      }
+    } else if (!numero.equals(other.numero)) {
+      return false;
     }
+    return true;
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        EmbeddedPaginaDiarioClinico other = (EmbeddedPaginaDiarioClinico) obj;
-        if (cfPaziente == null) {
-            if (other.cfPaziente != null)
-                return false;
-        } else if (!cfPaziente.equals(other.cfPaziente))
-            return false;
-        if (numero == null) {
-            if (other.numero != null)
-                return false;
-        } else if (!numero.equals(other.numero))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "EmbeddedPaginaDiarioClinico [cfPaziente=" + cfPaziente + ", numero=" + numero + "]";
-    }
+  @Override
+  public String toString() {
+    return "EmbeddedPaginaDiarioClinico [cfPaziente=" + cfPaziente + ", numero=" + numero + "]";
+  }
 }

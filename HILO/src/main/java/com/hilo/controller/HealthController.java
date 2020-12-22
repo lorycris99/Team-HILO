47561449786c062c9091616317e2c51527d58824
h3rm4nn-99 <73171414+h3rm4nn-99@users.noreply.github.com>
2020-12-22 11:19:31 +0100

    CheckStyle (parte 3)
    Ti prego non conflittare

--- HILO/src/main/java/com/hilo/controller/HealthController.java
+++ HILO/src/main/java/com/hilo/controller/HealthController.java
@@ -1,20 +1,12 @@
 package com.hilo.controller;
 
-<<<<<<< Updated upstream
-
-import com.hilo.model.HealthWorkerManagement.entity.HealthWorker;
-import com.hilo.model.HealthWorkerManagement.repository.HealthWorkerRepository;
+import com.hilo.model.healthworkermanagement.entity.HealthWorker;
+import com.hilo.model.healthworkermanagement.repository.HealthWorkerRepository;
 import java.util.List;
-=======
->>>>>>> Stashed changes
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-import com.hilo.model.healthworkermanagement.entity.HealthWorker;
-import com.hilo.model.healthworkermanagement.repository.HealthWorkerRepository;
 
 @RestController
 public class HealthController {
--- HILO/src/main/java/com/hilo/controller/StatisticsController.java
+++ HILO/src/main/java/com/hilo/controller/StatisticsController.java
@@ -1,8 +1,7 @@
 package com.hilo.controller;
 
 
-import com.hilo.model.StatisticsManagement.links.Links;
-
+import com.hilo.model.statisticsmanagement.Links;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/statistics/")
 public class StatisticsController {
-    @GetMapping(path = Links.GET_STATISTICS)
-    public ResponseEntity<?> listUsers() {
-        return (ResponseEntity<?>) ResponseEntity.ok();
-
-    }
+  @GetMapping(path = Links.GET_STATISTICS)
+  public ResponseEntity<?> listUsers() {
+    return (ResponseEntity<?>) ResponseEntity.ok();
+  }
 }

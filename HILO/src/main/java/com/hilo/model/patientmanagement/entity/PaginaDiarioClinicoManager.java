65c04639212d205a3cc54dd3c6747f6ee94a7d46
xXViRtUaLcObRaXx <65489866+xXViRtUaLcObRaXx@users.noreply.github.com>
2020-12-22 12:39:18 +0100

    Aggiunto PaginaDiarioClinicoManager
    Incapsulamento delle funzioni CRUD dell'entity PaginaDiarioClinico e aggiunta query alla repo

---
+++ HILO/src/main/java/com/hilo/model/patientmanagement/entity/PaginaDiarioClinicoManager.java
@@ -0,0 +1,51 @@
+package com.hilo.model.patientmanagement.entity;
+
+import com.hilo.model.patientmanagement.repository.PaginaDiarioClinicoRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class PaginaDiarioClinicoManager {
+
+  public PaginaDiarioClinicoManager(){
+
+  }
+
+  public void createPaginaDiarioClinico(PaginaDiarioClinico page) {
+    repo.save(page);
+  }
+
+  public void deletePaginaDiarioClinico(PaginaDiarioClinico page) {
+    repo.delete(page);
+  }
+
+  public void updatePaginaDiarioClinico(PaginaDiarioClinico page) {
+    createPaginaDiarioClinico(page);
+  }
+
+  public List<PaginaDiarioClinico> findAll() {
+    return repo.findAll();
+  }
+
+  public PaginaDiarioClinico findById(int id, String cf) {
+    EmbeddedPaginaDiarioClinico identifier = new EmbeddedPaginaDiarioClinico();
+    identifier.setNumero(id);
+    identifier.setCfPaziente(cf);
+
+    Optional<PaginaDiarioClinico> result = repo.findById(identifier);
+
+    if (result.isPresent()) {
+      return result.get();
+    } else {
+      return null;
+    }
+  }
+
+  public List<PaginaDiarioClinico> findByCf(String cf) {
+    return repo.findByCfPaziente(cf);
+  }
+
+  @Autowired
+  private PaginaDiarioClinicoRepository repo;
+}
--- HILO/src/main/java/com/hilo/model/patientmanagement/repository/PaginaDiarioClinicoRepository.java
+++ HILO/src/main/java/com/hilo/model/patientmanagement/repository/PaginaDiarioClinicoRepository.java
@@ -5,8 +5,11 @@ import com.hilo.model.patientmanagement.entity.PaginaDiarioClinico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PaginaDiarioClinicoRepository extends JpaRepository<PaginaDiarioClinico,
         EmbeddedPaginaDiarioClinico> {
+  List<PaginaDiarioClinico> findByCfPaziente(String cf);
 
 }

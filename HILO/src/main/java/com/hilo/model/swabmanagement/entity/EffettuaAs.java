0d6a6abf104369d62a21b9522506b2607b437f34
h3rm4nn-99 <73171414+h3rm4nn-99@users.noreply.github.com>
2020-12-23 13:04:05 +0100

    Update EffettuaAs.java
    Sorry (pt.2)

--- HILO/src/main/java/com/hilo/model/swabmanagement/entity/EffettuaAs.java
+++ HILO/src/main/java/com/hilo/model/swabmanagement/entity/EffettuaAs.java
@@ -8,7 +8,7 @@ import javax.persistence.Table;
 
 @Entity
 @IdClass(EmbeddedEffettuaAs.class)
-@Table(name = "effettua_p")
+@Table(name = "effettua_as")
 public class EffettuaAs {
   @Id
   @Column(name = "idtampone")

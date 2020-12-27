f2c4317634c2820d5ed9a8bb15657084d2456e3d
lost99 <ivancadamo@gmail.com>
2020-12-27 19:25:21 +0100

    Register Patient
    Non funzionante per problemi in afferire

--- HILO/src/main/java/com/hilo/controller/PatientController.java
+++ HILO/src/main/java/com/hilo/controller/PatientController.java
@@ -1,13 +1,14 @@
 package com.hilo.controller;
 
-import com.hilo.model.patientmanagement.entity.Pagina;
-import com.hilo.model.patientmanagement.entity.PaginaDiarioClinico;
-import com.hilo.model.patientmanagement.entity.PaginaDiarioClinicoManager;
-import com.hilo.model.patientmanagement.entity.PaginaManager;
-import com.hilo.model.patientmanagement.entity.Patient;
+import com.hilo.model.patientmanagement.entity.*;
+
 import java.util.List;
+
+import net.bytebuddy.utility.RandomString;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,23 @@ public class PatientController {
   private PaginaDiarioClinicoManager pdcm;
 
   @Autowired
+  private PatientManager patientManager;
+
+  @PostMapping("/patient/register")
+  public void registerPatient(@RequestParam(name = "User") String user) {
+    JSONObject obj = new JSONObject(user);
+    String cf = obj.getString("cf");
+    String name = obj.getString("name");
+    String surname = obj.getString("surname");
+    String username = RandomString.make(10);
+    String password = RandomString.make(10);
+    String mail = obj.getString("mail");
+    String telefono = obj.getString("telefono");
+    Boolean isInterno = obj.getBoolean("isInterno");
+    String indirizzo = obj.getString("indirizzo");
+    Patient p = new Patient(cf, username, password, mail, telefono, isInterno, indirizzo, name, surname);
+    patientManager.creaPaziente(p);
+  }
 
   @GetMapping("/patient/all")
   public List<Patient> getAll() {

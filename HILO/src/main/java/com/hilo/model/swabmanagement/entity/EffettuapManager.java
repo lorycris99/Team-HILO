dd4484c10abd73a8f3deaadcc68c94f1133757f8
xXViRtUaLcObRaXx <65489866+xXViRtUaLcObRaXx@users.noreply.github.com>
2020-12-27 13:15:53 +0100

    EffettuaP
    Aggiunte query ad EffettuaP

--- HILO/src/main/java/com/hilo/model/swabmanagement/entity/EffettuapManager.java
+++ HILO/src/main/java/com/hilo/model/swabmanagement/entity/EffettuapManager.java
@@ -1,6 +1,7 @@
 package com.hilo.model.swabmanagement.entity;
 
 import com.hilo.model.swabmanagement.repository.EffettuapRepository;
+import com.hilo.model.swabmanagement.repository.SwabRepository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Component;
 public class EffettuapManager {
   @Autowired
   private EffettuapRepository epr;
+  @Autowired
+  private SwabRepository swabRepo;
 
   public void createEffettuaP(EffettuaP ep) {
     epr.save(ep);
@@ -38,11 +41,48 @@ public class EffettuapManager {
   }
 
   public List<Integer> findTimestampBetween(String t1, String t2) {
-    List<EffettuaP> temp = epr.findByTimestampBetween(t1, t2);
-    List<Integer> list = new ArrayList<Integer>();
-    for (EffettuaP ep : temp) {
-      list.add(ep.getIdTampone());
+    if (t1.matches("\\d{4}-\\d{2}-\\d{2}") && t2.matches("\\d{4}-\\d{2}-\\d{2}")) {
+
+      List<EffettuaP> temp = epr.findByTimestampBetween(t1, t2);
+      List<Integer> list = new ArrayList<Integer>();
+      for (EffettuaP ep : temp) {
+        list.add(ep.getIdTampone());
+      }
+      return list;
+    } else {
+      throw new IllegalArgumentException("Una delle date non e' nel formato YYYY-MM-DD");
     }
-    return list;
+  }
+
+  public List<EffettuaP> findByGravityGreaterThan(double gravity) {
+    return epr.findByGravityGreaterThan(gravity);
+  }
+
+  public List<EffettuaP> findByTimestamp(String timestamp) {
+    if (timestamp.matches("\\d{4}-\\d{2}-\\d{2}")) {
+      return epr.findByTimestamp(timestamp);
+    } else {
+      throw new IllegalArgumentException("La data non e' nel formato YYYY-MM-DD");
+    }
+  }
+
+  public List<Swab> findByIdTampone(int id) {
+    List<EffettuaP> listaEff = epr.findByIdTampone(id);
+    List<Swab> listaTamponi = swabRepo.findAll();
+
+    List<Swab> filter = new ArrayList<>();
+
+    for (EffettuaP e : listaEff) {
+      int idTemp = e.getIdTampone();
+
+      for (int i = 0; i < listaTamponi.size(); i++) {
+        if (listaTamponi.get(i).getId() == idTemp) {
+          filter.add(listaTamponi.get(i));
+          break;
+        } //if
+      } //for interno
+    } //for esterno
+
+    return filter;
   }
 }
--- HILO/src/main/java/com/hilo/model/swabmanagement/repository/EffettuapRepository.java
+++ HILO/src/main/java/com/hilo/model/swabmanagement/repository/EffettuapRepository.java
@@ -9,4 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EffettuapRepository extends JpaRepository<EffettuaP, EmbeddedEffettuaP> {
   List<EffettuaP> findByTimestampBetween(String t1, String t2);
+  List<EffettuaP> findByGravityGreaterThan(double gravity);
+  List<EffettuaP> findByTimestamp(String timestamp);
+  List<EffettuaP> findByIdTampone(int idTampone);
 }

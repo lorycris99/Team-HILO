2ab06e2a3f66dd161fdcdd34f852e451490e77fa
lost99 <ivancadamo@gmail.com>
2021-01-02 12:56:33 +0100

    Code aggiuntivo

---
+++ Modulo IA/evaluate.py
@@ -0,0 +1,50 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+import pandas as pd
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import warnings
+warnings.filterwarnings('ignore')
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import EarlyStopping
+
+from tensorflow import keras
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+my_data_dir = '/mnt/c/Users/ivanc/Desktop/progetti/py/FIA/Data'
+test_path = my_data_dir+'/test/'
+
+image_shape = (100,100,1)
+image_gen = ImageDataGenerator(rotation_range=20,
+                               width_shift_range=0.10,
+                               height_shift_range=0.10,
+                               rescale=1/255,
+                               shear_range=0.1,
+                               zoom_range=0.1,
+                               horizontal_flip=True, 
+                               fill_mode='nearest')
+
+model = Sequential()
+
+model = keras.models.load_model('Ryan91,65')
+
+test_image_gen = image_gen.flow_from_directory(test_path,
+                                               target_size=(100,100),
+                                               color_mode='grayscale',
+                                               batch_size=64,
+                                               class_mode='categorical',shuffle=False)
+
+
+model.evaluate(test_image_gen)
+print(test_image_gen.class_indices)
---
+++ Modulo IA/oracle.py
@@ -0,0 +1,36 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+import pandas as pd
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import warnings
+warnings.filterwarnings('ignore')
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import EarlyStopping
+
+from tensorflow import keras
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+from tensorflow.keras.preprocessing import image
+
+test_image = image.load_img('prova/PNEUMONIA(3566).jpg', target_size=(100,100), color_mode='grayscale')
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image,axis=0)
+
+model = Sequential()
+
+model = keras.models.load_model('Ryan91,65')
+
+y = model.predict_proba(test_image, verbose=1)
+print("Predicted=%s" % (y[0]))

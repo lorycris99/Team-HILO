56d50c4db0951641aed7c0ff337c8312e85fcea6
h3rm4nn-99 <73171414+h3rm4nn-99@users.noreply.github.com>
2021-01-03 16:14:15 +0100

    Update oracle.py
    Generalizzazione per l'analisi di n radiografie nella stessa "sessione"

--- Modulo IA/oracle.py
+++ Modulo IA/oracle.py
@@ -1,5 +1,6 @@
 import numpy as np # linear algebra
 import sys
+import os
 import numpy as np 
 import warnings
 warnings.filterwarnings('ignore')
@@ -7,12 +8,13 @@ from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Sequential
 
-test_image = image.load_img('valuta/' + sys.argv[1], target_size=(100,100), color_mode='grayscale') #l'argomento da riga di comando rappresenta la radiografia da analizzare
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image,axis=0)
+for file in os.listdir("valuta/"):
+  test_image = image.load_img('valuta/' + file, target_size=(100,100), color_mode='grayscale')
+  test_image = image.img_to_array(test_image)
+  test_image = np.expand_dims(test_image,axis=0)
 
-model = Sequential()
-model = keras.models.load_model('Ryan92,5')
+  model = Sequential()
+  model = keras.models.load_model('Ryan92,5')
 
-y = model.predict_proba(test_image, verbose=1)
-print("Predicted=%s" % (y[0]))
+  y = model.predict_proba(test_image, verbose=1)
+  print("Predicted=%s" % (y[0]))

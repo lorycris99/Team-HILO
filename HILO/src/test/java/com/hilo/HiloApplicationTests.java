a79236363bbb6a46694129120549dd70b81069d0
h3rm4nn-99 <73171414+h3rm4nn-99@users.noreply.github.com>
2021-01-14 21:50:30 +0100

    Update HiloApplicationTests.java
    Implementati primi test tramite Test Suite

--- HILO/src/test/java/com/hilo/HiloApplicationTests.java
+++ HILO/src/test/java/com/hilo/HiloApplicationTests.java
@@ -1,13 +1,15 @@
 package com.hilo;
 
+import com.hilo.model.patientmanagement.entity.TestPatientManager;
+import com.hilo.model.swabmanagement.entity.SwabQueueProva;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HiloApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+@RunWith(Suite.class)
+@SuiteClasses({TestPatientManager.class, com.hilo.model.swab.SwabQueueProva.class})
+public class HiloApplicationTests {}
